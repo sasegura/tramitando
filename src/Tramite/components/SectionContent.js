3796d42a482be23a1738fd4642de634cb5180168
vladimir <vladimir.segura89@gmail.com>
2021-09-09 23:56:04 -0400

    Creado el componente para mostrar cada tramite con sus respectivas secciones.

--- src/Tramite/components/SectionContent.js
+++ src/Tramite/components/SectionContent.js
@@ -1,5 +1,5 @@
 export const SectionContent = (props) => {
-    const {type, content} = props.contentData;
+    const {type, content} = props.content;
 
     return <>{type === "text" ? <p>{content}</p> :
         <ul>{content.map((item) => {
--- src/pages/Tramites.js
+++ src/pages/Tramites.js
@@ -1,13 +1,28 @@
-import { Button } from "primereact/button";
+import {Button} from "primereact/button";
 import React from "react";
-import * as tramite from "../service/TramitesService.json";
-import { useHistory } from "react-router-dom";
+import * as tramitesJson from "../service/TramitesService.json";
+import {useHistory} from "react-router-dom";
 import {SectionTitle} from "../Tramite/components/SectionTitle";
-import {SectionContent} from "../Tramite/components/SectionContent";
+import {FormalitiesContainer} from "../Tramite/components/FormalitiesContainer";
+
 export const Tramites = () => {
-    const { tramites, tramitesServices } = tramite;
+    const {tramites, tramitesServices} = tramitesJson;
     const history = useHistory();
-    const data = [
+    const data = {
+        "title": "Firma electrónica",
+        "sections": [
+            {
+                "title": "¿Qué requisitos necesito para renovar el NIE?",
+                "contents": [
+                    {
+                        "type": "text",
+                        "content": "La renovación del NIE es uno de los...."
+                    }
+                ]
+            },
+            {
+                "title": "¿Dónde puedo renovarlo?",
+                "contents": [
                     {
                         "type": "text",
                         "content": "Actualmente, tienes varios...."
@@ -24,31 +39,62 @@ export const Tramites = () => {
                         "type": "text",
                         "content": "Desde 2016, puedes...."
                     }
-                ];
+                ]
+            }
+        ],
+        "guided_help": {
+            "pageNumber": 4,
+            "pages": [
+                {
+                    "title": "asd",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "content": "dasdasd"
+                        },
+                        {
+                            "type": "list",
+                            "content": [
+                                "asdadasd",
+                                "asdasd"
+                            ]
+                        },
+                        {
+                            "type": "text",
+                            "content": "asdasd"
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+
     return (
         <div className="p-grid">
             <div className="p-col-12 p-d-flex">
                 <div className="p-col-1"></div>
                 <div className="card p-col-10">
+                    <FormalitiesContainer data={data}/>
+                    <FormalitiesContainer data={data}/>
                     {/*<h5>Trámites</h5>*/}
                     <SectionTitle title="Trámites"/>
                     {tramitesServices.map((tramite) => {
                         return (
                             <li>
-                                <Button className="p-button-text" onClick={() => history.push(tramite.url)} label={tramite.label} />
+                                <Button className="p-button-text" onClick={() => history.push(tramite.url)} label={tramite.label}/>
                             </li>
                         );
                     })}
-                    <SectionTitle title="Ayudas"/>
-                    {data.map( obj => {
-                        return <SectionContent contentData={obj}/>;
-                    })
-                    }
+                    {/*<SectionTitle title="Ayudas"/>*/}
+                    {/*{data.map(obj => {*/}
+                    {/*    return <SectionContent contents={obj}/>;*/}
+                    {/*})*/}
+                    {/*}*/}
                     {/*<h5>Ayudas</h5>*/}
                     {tramites.map((tramite) => {
                         return (
                             <li>
-                                <Button className="p-button-text" onClick={() => history.push(tramite.url)} label={tramite.label} />
+                                <Button className="p-button-text" onClick={() => history.push(tramite.url)} label={tramite.label}/>
                             </li>
                         );
                     })}
